6b4390c1283b7d3a7a66d601a80c7e6ed4c89b90
Alex Mykhailov <arizel022@gmail.com>
2023-02-21 21:30:13 +0300

    full index

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,3 +1,21 @@
 $(function() {
 
+
+
+    $('.hero__slider').slick({
+        dots: false,
+        infinite: true,
+        arrows: false,
+        autoplaySpeed: 10000,
+        autoplay: true,
+        fade: true
+    });
+
+    $('.reviews__inner').slick({
+        dots: true,
+        arrows: true
+    });
+    
+
+
 });
