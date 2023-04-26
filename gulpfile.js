518f61e65fe193d67a4607b84faca1f0aeeaf21e
Alex Mykhailov <arizel022@gmail.com>
2023-04-27 02:42:47 +0300

    4

--- gulpfile.js
+++ gulpfile.js
@@ -38,10 +38,10 @@ function scripts() {
         
         'node_modules/jquery/dist/jquery.js',
 
-        // 'node_modules/slick-carousel/slick/slick.js',
-        // 'node_modules/mixitup/dist/mixitup.js',
-        // 'app/js/colorbox.js',
-        // 'node_modules/magnific-popup/dist/jquery.magnific-popup.js'
+        'node_modules/slick-carousel/slick/slick.js',
+        'node_modules/mixitup/dist/mixitup.js',
+        'app/js/colorbox.js',
+        'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
 
         'app/js/main.js'
 
